8b410252f4b3f7dccfce7ba3207b35f293317637
JackonYang <jiekunyang@gmail.com>
2013-03-05 13:16:50 +0800

    add watch and update readme to fit ../watch.sh cmd [filenamePre]

---
+++ watch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function start {
+	#usage ./watch_start filePrefix runtime
+
+	inteval=1
+	filePrefix=$1
+	timestamp=`date +%m%d%H%M`
+
+	cpuFile=`echo $filePrefix\_$timestamp\_cpu.txt`
+	memFile=`echo $filePrefix\_$timestamp\_mem.txt`
+	#ioFile=`echo $filePrefix\_$timestamp\_io.txt`
+
+	sar $inteval >> $cpuFile &
+	sar -r $inteval >> $memFile &
+}
+
+function stop {
+	pid=`ps -ef |grep sar|grep -v grep| awk '{print $2}'`
+	for i in $pid
+	do
+		kill -9 $i
+	done
+}
+
+function clean {
+	rm -f *_cpu.txt *_mem.txt
+}
+
+function show {
+	python3 show.py $1
+}
+
+# cmd, filename_pre
+$1 $2
