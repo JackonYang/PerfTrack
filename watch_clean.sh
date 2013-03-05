3f2c6f09ed4ec11d6524dbdeefccf5a342df6d57
JackonYang <jiekunyang@gmail.com>
2013-03-05 13:11:39 +0800

    refactor, one shell script with cmd parameters

--- watch_clean.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-#exec 2>/dev/null
-
-rm -f *_cpu.txt *_mem.txt
--- watch_stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pid=`ps -ef |grep sar|grep -v grep| awk '{print $2}'`
-for i in $pid
-do
-	kill -9 $i
-done
