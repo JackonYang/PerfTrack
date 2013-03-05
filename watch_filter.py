b65408f327f2453962642007396c4e41a7d983a4
JackonYang <jiekunyang@gmail.com>
2013-03-05 12:42:24 +0800

    refactor, filter/read_data/data_to_show/main functions

--- watch_filter.py
+++ watch_filter.py
@@ -1,118 +1,91 @@
 #show cpu and memory runtime infomation
-import filtration
-
 import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+def keep_peak(orig_data,step=7,peak_range=5):
+	"""wave filtration
 
-def keepPeak(data,colsToFilter):
-	#one of wave filtration methods
-	#input: a list and colsTofilter
-	#output: return a list contains data of  colsTofilter.
-	# each column two data
-	#if max-min <= 5, return 2 middle data
-	#else return max and min row
+	if max-min <= peak_range, return 2 middle data
+	else return max and min row"""
 
-	delta=5 #peak range
-	nCols=len(colsToFilter)
-	#copy userful data to a new list in columns
-	colData=[]
-	for i in range(nCols):
-		colData.append([])
-
-	for line in data:
-		line=line.split()
-		for i in range(nCols):
-			colData[i].append(line[colsToFilter[i]])
-	#choose 2 data for each column
 	res=[]
-	for idata in colData:
-		maxData=max(idata)
-		minData=min(idata)
-		if (float(maxData)-float(minData)>delta):
-			if(idata.index(maxData)<idata.index(minData)):
-				res.append([maxData,minData])
-			else:
-				res.append([minData,maxData])
+	for i in range(0,len(orig_data),step):
+		cut=orig_data[i:i+step]
+		cut.sort()
+		if float(cut[-1])-float(cut[0])<peak_range:
+			# if delta is small, take the two middle value.
+			n=int(step/2)
+			res.extend(orig_data[int(step/2)-1:int(step/2)+1])
 		else:
-			idata.sort()
-			n=len(idata)/2
-			res.append(idata[n-1:n+1])
+			# take the max and min in the orig sequence
+			if orig_data[i:i+step].index(cut[0])<orig_data[i:i+step].index(cut[-1]):
+				res.extend([cut[0],cut[-1]])
+			else:
+				res.extend([cut[-1],cut[0]])
 	return res
 
-def readData(filename,nskip,dataToRead):
-	""" read columns of dataToRead from filename
-	return a list of data, each dataToRead a column
-	considering the amount of memory, keep peak filteration is used"""
+def read_data(filename,dataToRead=None):
+	""" read performance data from files, 
+	return a dictionary(performance_index:data)"""
 
-	f=open(filename,'r')
-	#skip n lines
-	while(nskip):
-		f.readline()
-		nskip=int(nskip)-1
-	#get 1st line to init the title
-	titleline=f.readline().split()
-	#init the 1st line of data with title
-	data=[]
-	for i in range(len(dataToRead)):
-		data.append([])
-		data[i].append(titleline[dataToRead[i]])
+	with open(filename,'r') as f:
+		def get_title(f):
+			"""skip line before title, and read title line"""
+			line=f.readline()
+			while '%' not in line:
+				line=f.readline()
+			else:
+				title=line.split()
+				title[0]='time'
+			return title
 
-	#read data
-	dataBuffer=f.readlines(1)
-	while (len(dataBuffer)!=0):
-		#every 7 rows a group to filter, 
-		#extend filter result list to data list
-		filterStep=7
-		for i in range(0,len(dataBuffer),filterStep):#[:-1]:
-			filterRes=filtration.keepPeak(dataBuffer[i:i+filterStep],dataToRead)
-			for j in range(len(dataToRead)):
-				data[j].extend(filterRes[j])
-		dataBuffer=f.readlines(1)
+		# init title tag
+		title=get_title(f)
+		data={}
+		for tag in title:
+			data[tag]=[]
+		# read data
+		for line in f.readlines():
+			for tag,value in zip(title,line.split()):
+				data[tag].append(value)
 	return data
 
-def plot():
+def plot(data):#data:dict
 	"""plot data"""
-	global colsCpu
-	global colsMem
 
-	title=[]
-	for i in range(len(colsCpu)):
-		title.append(cpudata[i].pop(0))
-		plt.plot(cpudata[i])
-
-	for i in range(len(colsMem)):
-		title.append(memdata[i].pop(0))
-		plt.plot(memdata[i])	
-	plt.legend(title)
-#plt.title("cpu and memory runtime info")
+	for y in data.values():
+		plt.plot(y)
+	plt.legend(data.keys())
+	plt.title("cpu and memory performance figure")
 	plt.show()
 
-timestamp=sys.argv[1]
+def data_to_show(data):
+	"""format useful info of cpu performance"""
+	res={}
+	for key in set(data.keys() & {'%user','%usr','%system','%sys','%idle','%memused'}):
+		res[key]=keep_peak(data[key])
 
-#line to skip
-nskip=2
-#cpu col
-colsCpu=[2,4,7]
-colsMem=[3,7]
-idxIdle=2
+	return res
+
+def main(filename_pre='test_03041503'):
+	filterd={}
+	filterd.update(data_to_show(read_data(filename_pre+'_cpu.txt')))
+	filterd.update(data_to_show(read_data(filename_pre+'_mem.txt')))
+	plot(filterd)
+
+filename_pre=sys.argv[1]
+main(filename_pre)
+
+#idxIdle=2
 
 #path of data files
-s=os.getcwd()
+#s=os.getcwd()
 #data files to plot
-files=[files for files in os.listdir(s) if (files.find(timestamp) > -1)]
-
-for datafile in files:
-	if(datafile.find("cpu") != -1):
-		cpudata=readdata.readData(datafile,nskip,colsCpu)
-	if(datafile.find("mem") != -1):
-		memdata=readdata.readData(datafile,nskip,colsMem)
+#files=[files for files in os.listdir(s) if (files.find(timestamp) > -1)]
 
 #cpu idle operate
-cpudata[idxIdle][0]="%totalCPUUsed"
-for i in range(1,len(cpudata[idxIdle])):
-	cpudata[idxIdle][i]=100-float(cpudata[idxIdle][i])
-
-plot()
+#cpudata[idxIdle][0]="%totalCPUUsed"
+#for i in range(1,len(cpudata[idxIdle])):
+#	cpudata[idxIdle][i]=100-float(cpudata[idxIdle][i])
