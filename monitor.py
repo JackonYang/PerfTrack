83a56a9e2237eee1aba69372d560c80f99843aef
JackonYang <jiekunyang@gmail.com>
2014-07-17 23:36:44 +0800

    sample watch by seconds

--- monitor.py
+++ monitor.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8-*-
 import psutil
+import time
 
-def get_proc(pname):
+def get_proc_by_name(pname):
     """ get process by name
     
-    return the 1st process if there are more than one
+    return the first process if there are more than one
     """
-    procs = psutil.get_process_list()
-    for proc in procs:
-        if proc.name().lower() == pname.lower():
-            return proc
+    for proc in psutil.process_iter():
+        try:
+            if proc.name().lower() == pname.lower():
+                return proc  # return if found one
+        except psutil.AccessDenied:
+            pass
+        except psutil.NoSuchProcess:
+            pass
     return None
 
+def run(proc, interval=1):
+    while(True):
+        print proc.memory_info().rss
+        time.sleep(interval)
+
 if '__main__' == __name__:
-    print get_proc("CHrome")
-    print get_proc("gnome-terminal")
-    print get_proc("python")
+    proc = get_proc_by_name("CHrome")
+    run(proc)
