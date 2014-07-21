442750031f9871659d0421f59b57bdd699062805
JackonYang <jiekunyang@gmail.com>
2014-07-21 22:09:36 +0800

    auto show matplotlib in GUI

--- perf_track.py
+++ perf_track.py
@@ -2,11 +2,19 @@
 import wx
 import monitor
 
+from matplotlib.figure import Figure
+from matplotlib import font_manager
+import numpy as np
+from matplotlib.backends.backend_wxagg import \
+  FigureCanvasWxAgg as FigureCanvas
+
 
 __app_name__ = 'PerfTrack'
 __author__ = 'Jackon Yang'
 __email__ = 'jiekunyang@gmail.com'
 
+TIMER_ID = wx.NewId()
+POINTS = 300
 
 def format_proc(proc):
     """brief description of proc in string format
@@ -27,6 +35,7 @@ class MonitorFrame(wx.Frame):
         self.proc_tracking = None
         self.proc_running = False
         self.proc_name_value.SetFocus()
+        self.t = wx.Timer(self, TIMER_ID)
 
     def BuildUI(self):
         # ------- config box ------------
@@ -41,7 +50,7 @@ class MonitorFrame(wx.Frame):
         # add to config box
         self.configBox= wx.BoxSizer(wx.VERTICAL)
         self.configBox.AddSpacer(10)
-        self.configBox.Add(self.proc_name_box, 0, wx.LEFT, 5, 0)
+        self.configBox.Add(self.proc_name_box, 1, wx.LEFT, 5, 0)
         self.configBox.AddSpacer(10)
         self.configBox.Add(self.proc_msg, 1, wx.LEFT, 5, 0)
 
@@ -64,10 +73,17 @@ class MonitorFrame(wx.Frame):
         # ------- track log box -------------------
         self.track_log = wx.TextCtrl(parent=self, style=wx.TE_AUTO_SCROLL | wx.TE_MULTILINE)
         self.track_log.SetEditable(False)
+        self.fig = Figure((6, 4), 100)
+        # bind the Figure to the backend specific canvas
+        self.canvas = FigureCanvas(self, wx.ID_ANY, self.fig)
+        self.InitPlotUI()
+        self.dispbox = wx.BoxSizer(wx.HORIZONTAL)
+        self.dispbox.Add(self.track_log, 1, wx.ALL|wx.EXPAND, 5, 5)
+        self.dispbox.Add(self.canvas, 5, wx.ALL|wx.EXPAND, 5, 5)
         # ------- main box(tool, tracklog) --------
         self.mainbox = wx.BoxSizer(wx.VERTICAL)
-        self.mainbox.Add(self.toolbox, 0, wx.NORMAL, 0, 0)
-        self.mainbox.Add(self.track_log, 1, wx.ALL | wx.EXPAND, 5, 5)
+        self.mainbox.Add(self.toolbox, 1, wx.NORMAL, 0, 0)
+        self.mainbox.Add(self.dispbox, 5, wx.ALL|wx.EXPAND, 5, 5)
 
         self.SetSizer(self.mainbox)
         self.CenterOnScreen()
@@ -110,6 +126,8 @@ class MonitorFrame(wx.Frame):
         self.mem_watcher.start()
         self.proc_running = True
         # deal with close proc while monitoring
+        # plot
+        self.t.Start(50)
 
     def OnStopTrack(self, event):
         self.startBtn.Enable()
@@ -117,6 +135,7 @@ class MonitorFrame(wx.Frame):
         self.stopBtn.Disable()
         # stop thread
         self.mem_watcher.stop()
+        self.t.Stop()
         self.proc_running = False
 
     def OnWindowActivate(self, event):
@@ -142,6 +161,50 @@ class MonitorFrame(wx.Frame):
             self.proc_msg.SetLabel(format_proc(self.proc_tracking))
         return self.proc_tracking
 
+    def InitPlotUI(self):
+        # add a subplot
+        self.ax = self.fig.add_subplot(111)
+        # limit the X and Y axes dimensions
+        self.ax.set_ylim([0, 100])
+        self.ax.set_xlim([0, POINTS])
+       
+        self.ax.set_autoscale_on(False)
+        self.ax.set_xticks([])
+        # we want a tick every 10 point on Y (101 is to have 10
+        self.ax.set_yticks(range(0, 101, 10))
+        # disable autoscale, since we don't want the Axes to ad
+        # draw a grid (it will be only for Y)
+        self.ax.grid(True)
+        # generates first "empty" plots
+        self.user = [None] * POINTS
+        self.l_user,=self.ax.plot(range(POINTS),self.user,label='User %')
+
+        # add the legend
+        self.ax.legend(loc='upper center',
+                           ncol=4,
+                           prop=font_manager.FontProperties(size=10))
+        # force a draw on the canvas()
+        # trick to show the grid and the legend
+        self.canvas.draw()
+        # save the clean background - everything but the line
+        # is drawn and saved in the pixel buffer background
+        self.bg = self.canvas.copy_from_bbox(self.ax.bbox)
+        # bind events coming from timer with id = TIMER_ID
+        # to the onTimer callback function
+        wx.EVT_TIMER(self, TIMER_ID, self.onTimer)
+
+    def onTimer(self, evt):
+        """callback function for timer events"""
+        # restore the clean background, saved at the beginning
+        self.canvas.restore_region(self.bg)
+                # update the data
+        temp =np.random.randint(10,80)
+        self.user = self.user[1:] + [temp]
+        # update the plot
+        self.l_user.set_ydata(self.user)
+        # just draw the "animated" objects
+        self.ax.draw_artist(self.l_user)# It is used to efficiently update Axes data (axis ticks, labels, etc are not updated)
+        self.canvas.blit(self.ax.bbox)
 
 class MonitorUI(wx.App):
 
